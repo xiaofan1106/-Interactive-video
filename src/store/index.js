ba501a73c4cbfdaf54bcbd3641ab92b31d5df50c
xiaofan1106 <weijiefan@tencent.com>
2020-07-19 12:55:31 +0800

    home

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 电影列表
+    movies: [
+      {
+        name: "test1",
+        url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+      },
+      {
+        name: "test2",
+        url: "http://vjs.zencdn.net/v/oceans.mp4"
+      },
+      {
+        name: "test3",
+        url: "https://media.w3.org/2010/05/sintel/trailer.mp4"
+      },
+      {
+        name: "test4",
+        url:
+          "http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4"
+      }
+    ]
   },
   mutations: {
   },
--- src/views/interactive.vue
+++ src/views/interactive.vue
@@ -19,3 +19,4 @@ export default {
 </script>
 <style lang="scss" scoped>
 </style>
+ 
